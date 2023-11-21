6ceb32f5b3ce5d4700b18ade9527506137f84c42
Anicet OTT-FLO <anicet.ottmann-florentin@epitech.eu>
2023-11-21 17:40:38 +0100

    Fix integer overflow on too big inputs

--- board.c
+++ board.c
@@ -11,36 +11,39 @@ void board_destroy(board_t board)
     my_free(board);
 }
 
-static int does_square_fit(board_t board, int square_size, int x, int y)
+static int does_square_fit(
+    board_t board, size_t square_size, size_t x, size_t y)
 {
     if (x + square_size > board->width || y + square_size > board->height)
         return 0;
-    for (int i = 0; i < square_size && x + i < board->width; i++)
-        for (int j = 0; j < square_size && y + j < board->height; j++)
+    for (size_t i = 0; i < square_size && x + i < board->width; i++)
+        for (size_t j = 0; j < square_size && y + j < board->height; j++)
             if (board->cells[(y + j) * board->width + x + i] != '.')
                 return 0;
     return 1;
 }
 
-static int does_square_fit_rim(board_t board, int square_size, int x, int y)
+static int does_square_fit_rim(
+    board_t board, size_t square_size, size_t x, size_t y)
 {
-    for (int i = 0; i < square_size && x + i < board->width; i++)
+    for (size_t i = 0; i < square_size && x + i < board->width; i++)
         if (board->cells[(y + square_size - 1) * board->width + x + i] != '.')
             return 0;
-    for (int i = 0; i < square_size && y + i < board->height; i++)
+    for (size_t i = 0; i < square_size && y + i < board->height; i++)
         if (board->cells[(y + i) * board->width + x + square_size - 1] != '.')
             return 0;
     return 1;
 }
 
-static void fill_square(board_t board, int square_size, int x, int y)
+static void fill_square(board_t board, size_t square_size, size_t x, size_t y)
 {
-    for (int i = 0; i < square_size; i++)
-        for (int j = 0; j < square_size; j++)
+    for (size_t i = 0; i < square_size; i++)
+        for (size_t j = 0; j < square_size; j++)
             board->cells[(y + i) * board->width + x + j] = 'x';
 }
 
-int find_biggest_square_at(board_t board, int *square_size, int x, int y)
+int find_biggest_square_at(
+    board_t board, size_t *square_size, size_t x, size_t y)
 {
     int ret = 0;
 
@@ -54,10 +57,11 @@ int find_biggest_square_at(board_t board, int *square_size, int x, int y)
     return ret;
 }
 
-void find_biggest_square(board_t board, int *square_size, int *x, int *y)
+void find_biggest_square(
+    board_t board, size_t *square_size, size_t *x, size_t *y)
 {
-    for (int i = 0; i < board->height; i++)
-        for (int j = 0; j < board->width; j++) {
+    for (size_t i = 0; i < board->height; i++)
+        for (size_t j = 0; j < board->width; j++) {
             if (find_biggest_square_at(board, square_size, j, i)) {
                 *x = j + 1;
                 *y = i + 1;
@@ -67,9 +71,9 @@ void find_biggest_square(board_t board, int *square_size, int *x, int *y)
 
 void board_resolve(board_t board)
 {
-    int square_size = 0;
-    int x = 0;
-    int y = 0;
+    size_t square_size = 0;
+    size_t x = 0;
+    size_t y = 0;
 
     find_biggest_square(board, &square_size, &x, &y);
     fill_square(board, square_size, x - 1, y - 1);
@@ -77,13 +81,13 @@ void board_resolve(board_t board)
 
 void board_display(const board_t board)
 {
-    for (int i = 0; i < board->height; i++) {
+    for (size_t i = 0; i < board->height; i++) {
         write(STDOUT_FILENO, board->cells + i * board->width, board->width);
         write(STDOUT_FILENO, "\n", 1);
     }
 }
 
-static board_t board_construct(int width, int height)
+static board_t board_construct(size_t width, size_t height)
 {
     board_t board = my_malloc(sizeof(*board));
 
@@ -94,21 +98,20 @@ static board_t board_construct(int width, int height)
     return board;
 }
 
-board_t board_generate(int size, const char *pattern)
+board_t board_generate(size_t size, const char *pattern)
 {
     board_t board = board_construct(size, size);
 
-    for (int i = 0; pattern[i] != '\0'; i++)
+    for (size_t i = 0; pattern[i] != '\0'; i++)
         if (pattern[i] != '.' && pattern[i] != 'o')
             return NULL;
-    for (int i = 0; i < (size * size) / (int) my_strlen(pattern); i++)
+    for (size_t i = 0; i < (size * size) / my_strlen(pattern); i++)
         my_memcpy(board->cells + i * my_strlen(pattern), pattern,
             my_strlen(pattern));
-    if ((size * size) % (int) my_strlen(pattern) != 0)
+    if ((size * size) % my_strlen(pattern) != 0)
         my_memcpy(board->cells
-                + (size * size) / (int) my_strlen(pattern)
-                    * my_strlen(pattern),
-            pattern, (size * size) % (int) my_strlen(pattern));
+                + (size * size) / my_strlen(pattern) * my_strlen(pattern),
+            pattern, (size * size) % my_strlen(pattern));
     return board;
 }
 
@@ -121,9 +124,10 @@ static size_t get_file_size(const char *filename)
     return st.st_size;
 }
 
-static int guess_size(int *width, int *height, char **line, char *buffer)
+static int guess_size(
+    ssize_t *width, size_t *height, char **line, char *buffer)
 {
-    int read_size = *width;
+    ssize_t read_size = *width;
 
     for (ssize_t i = 0; i < read_size; i++)
         if (buffer[i] == '\n') {
@@ -139,9 +143,9 @@ static int guess_size(int *width, int *height, char **line, char *buffer)
     return 0;
 }
 
-int is_line_valid(char *line, int width)
+int is_line_valid(char *line, size_t width)
 {
-    for (int i = 0; i < width; i++)
+    for (size_t i = 0; i < width; i++)
         if (line[i] != '.' && line[i] != 'o')
             return 0;
     return 1;
@@ -153,8 +157,8 @@ board_t board_load_from_file(const char *filename)
     board_t board = NULL;
     char *buffer = NULL;
     char *line = NULL;
-    int height = 0;
-    int width = 0;
+    size_t height = 0;
+    ssize_t width = 0;
 
     buffer = my_malloc(get_file_size(filename));
     if (fd == -1)
@@ -164,10 +168,10 @@ board_t board_load_from_file(const char *filename)
         return NULL;
     guess_size(&width, &height, &line, buffer);
     board = board_construct(width, height);
-    for (int i = 0; i < height; i++) {
+    for (size_t i = 0; i < height; i++) {
         if (line[i * (width + 1) + width] != '\n')
             return NULL;
-        if ((i + 1) * width + (line - buffer) > (long) get_file_size(filename))
+        if ((i + 1) * width + (line - buffer) > get_file_size(filename))
             return NULL;
         if (is_line_valid(line + i * (width + 1), width))
             my_memcpy(
--- board.h
+++ board.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <stddef.h>
 typedef struct board {
-    int width;
-    int height;
+    size_t width;
+    size_t height;
     char *cells;
 } *board_t;
 
 board_t board_load_from_file(const char *filename);
-board_t board_generate(int size, const char *pattern);
+board_t board_generate(size_t size, const char *pattern);
 void board_destroy(board_t board);
 void board_display(const board_t board);
 void board_resolve(board_t board);
